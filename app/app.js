599098a6a7fcc02f66e613041d230b327f049999
Koterlan David <koterlandavid@gmail.com>
2024-07-24 13:33:03 +0200

    Add app.js

---
+++ app/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 8080;
+const host = process.env.HOST || 'localhost'
+
+app.get('/', (_, res) => {
+    res.send(`Hello World!`);
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://${host}:${port}/`);
+});
