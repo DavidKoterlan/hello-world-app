fcafea64c14c141008e874a177088e1823b38781
Koterlan David <koterlandavid@gmail.com>
2024-07-25 08:44:28 +0200

    Change  "/" endpoint response message in app.js

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080;
 const host = process.env.HOST || 'localhost'
 
 app.get('/', (_, res) => {
-    res.send(`Hello World!`);
+    res.send(`Hello, World!`);
 });
 
 app.listen(port, () => {
